0b8654f1ff79ccd0adefee034e1869ab4a31ea40
venkateshkoka <venkateshkoka@ccs.neu.edu>
2017-05-22 11:44:31 -0400

    modifying server.js

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.listen(port);*/
 
 var express = require('express');
 var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // --------- this is called routing
 /*app.get('/hello',function (req, res) {
     console.log();
@@ -25,4 +30,8 @@ var app = express();
 })*/
 
 app.use(express.static(__dirname + '/public/'));
-app.listen(3000);
+
+require ("./test/app.js")(app);
+
+var port = process.env.PORT || 3000;
+app.listen(port);
